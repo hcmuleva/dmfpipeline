f3ccda7a96b4bae2ceb6f5cc42c4bc80a298129a
Harish Muleva <Harish.Muleva@dell.com>
2022-03-12 13:47:38 +0530

    first maven project with Unit Test cases

---
+++ src/main/java/hcmdemo/App.java
@@ -0,0 +1,19 @@
+package hcmdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{   
+    public int add(int x, int y){
+        return x+y;
+    }
+    public int sub(int x, int y){
+        return x-y;
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
